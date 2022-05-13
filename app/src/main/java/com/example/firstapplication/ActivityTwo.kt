3ba89277451610968f59732164b66d3e84b06a6b
Gorelow <gorantmi49@gmail.com>
2022-05-13 12:18:43 +0300

    this is my app

--- app/src/main/java/com/example/firstapplication/ActivityTwo.kt
+++ app/src/main/java/com/example/firstapplication/ActivityTwo.kt
@@ -1,10 +1,12 @@
 package com.example.firstapplication
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.firstapplication.databinding.ActivityTwoBinding
-import com.example.firstapplication.databinding.ItemCellBinding
 import com.example.firstapplication.model.CellAdapter
 import com.example.firstapplication.model.CellService
+import com.example.firstapplication.model.CellsListener
 
 class ActivityTwo : AppCompatActivity() {
 
@@ -13,4 +15,28 @@ class ActivityTwo : AppCompatActivity() {
 
     private val cellService : CellService
     get() = (applicationContext as App).cellService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTwoBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_two)
+
+        adapter = CellAdapter()
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = adapter
+
+        cellService.AddListener(cellsListener)
+    }
+
+
+    //override fun OnDestroy() {
+    //    super.OnDestroy()
+    //    cellService.RemoveListener(cellsListener)
+    //}
+
+    private val cellsListener : CellsListener = {
+        adapter.cells = it
+    }
 }
